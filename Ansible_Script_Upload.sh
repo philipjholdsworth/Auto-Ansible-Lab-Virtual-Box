9f4671d7a5902bdc40a52e0061e0810d2138cbd6
philipjholdsworth <43854415+philipjholdsworth@users.noreply.github.com>
2022-09-06 13:54:26 +0100

    Add files via upload

--- Ansible_Script_Upload.sh
+++ Ansible_Script_Upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #This script allows for playbook uploads to the Controller VM
 vagrant ssh-config > vagrant-ssh
-scp -F vagrant-ssh Playbooks/Reboot_IF_Required.yml Playbooks/Linux_Reboot.yml Playbooks/Ping_All.yml controller:/home/vagrant/ansible_project
+scp -F vagrant-ssh Playbooks/Reboot_IF_Required.yml Playbooks/Linux_Reboot.yml Playbooks/Ping_All.yml Playbooks/Linux_Update_Start.yml Playbooks/Linux_Dist_Update_Start.yml Playbooks/Docker_Prevision.yml controller:/home/vagrant/ansible_project
 exit
